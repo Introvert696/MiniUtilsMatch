8a50292604b0d64d7354fcc2040f2e3174a6389b
Evgeniy <36341190+Introvert696@users.noreply.github.com>
2020-04-01 15:07:51 +0400

    Вау
    Вместо простых опираций добавил функции

--- firstutil.py
+++ firstutil.py
@@ -1,5 +1,49 @@
 from math import *
 
+############################
+def slozhenie(a,b):
+    result = a + b
+    res = str(result)
+    print("Ответ: ", res)
+############################
+def otnyatie(a,b):
+    result = a - b
+    res = str(result)
+    print("Ответ: ", res)
+############################
+def raznost(a,b):
+    result = a / b
+    res = str(result)
+    print("Ответ :", res)
+############################
+def umnozhenie(a,b):
+    result = a * b
+    res = str(result)
+    print("Ответ: ", res)
+#############################
+def pifagor(a,b):
+    cen = float(sqrt(a**2 + b**2))
+    cen = round(cen, 3)                 #Округление до 3 знаков после точки
+    c = str(cen)
+    print("Результат: ",c)
+###############################
+def logarifm(x,y):
+    resultlog = float(log(x, y))
+    resultlog = round(resultlog, 3)
+    relog = str(resultlog)
+    print("Result", relog)
+################################
+def ploshad(a,b,c,d):
+    ploshad = a * b * c * d
+    ploshad = str(ploshad)
+    print("Результат: ",ploshad)
+#################################
+def ploshadkrug(res):
+    pis = 3.14
+    otvet = float(2 * pis * res)
+    otvet = str(otvet)
+    print("Радиус приблизительно: ",otvet)
+
 print("Выберете что вам нужно")
 print("1.Простое вычисление")
 print("2.Разные формулы") #Допилить другие функции
@@ -12,17 +56,13 @@ if(vvod == 1):
     print("1.Сложить \n2.Отнять \n3.Разделить \n4.Умножить")
     vvod = int(input("> "))
     if(vvod == 1):                  #НАЧАЛО Все операции
-        otvet = num_1 + num_2
-        print(otvet)
+        slozhenie(num_1,num_2)
     elif (vvod == 2):
-        otvet = num_1 - num_2
-        print(otvet)
+        otnyatie(num_1,num_2)
     elif (vvod == 3):
-        otvet = num_1 / num_2
-        print(otvet)
+        raznost(num_1,num_2)
     elif (vvod == 4):
-        otvet = num_1 * num_2
-        print(otvet)
+        umnozhenie(num_1,num_2)
     else:
         print("Netu")                   #КОНЕЦ Все операции
 elif(vvod == 2):
@@ -32,35 +72,25 @@ elif(vvod == 2):
     vvod1 = int(input("> "))
     if (vvod1 == 1):
         print('Введите 2 стороны прямоугольного треугольника\n')
-        a = int(input('a = \n'))
-        b = int(input('b = '))
-        cen = float(sqrt(a**2 + b**2))
-        cen = round(cen, 3)                 #Округление до 3 знаков после точки
-        c = str(cen)
-        print("Результат: ",c) 
+        a = int(input('a = '))
+        b = int(input('b = ')) 
+        pifagor(a,b)
     elif (vvod1 == 2):
         print('Логарифм числа x по основанию y\n')
         x = int(input('Введите x: \n> '))
         y = int(input('Введите y: \n> '))
-        resultlog = float(log(x, y))
-        relog = str(resultlog)
-        print("Result", relog)
+        logarifm(x,y)
     elif (vvod1 == 3):
         print('Площадь 4-х угольника \nДля начала введите ваши 4 стороны')
         a = int(input("Введите 1 сторону: "))
         b = int(input("Введите 2 сторону: "))
         c = int(input("Введите 3 сторону: "))
         d = int(input("Введите 4 сторону: "))
-        ploshad = a * b * c * d
-        ploshad = str(ploshad)
-        print("Результат: ",ploshad)
+        ploshad(a,b,c,d)
     elif (vvod1 == 4):
         print('Площадь круга')
         res = int(input("Введите радиус круга: "))
-        pis = 3.14
-        otvet = float(2 * pis * res)
-        otvet = str(otvet)
-        print("Радиус приблизительно: ",otvet)
+        ploshadkrug(res)
     elif (vvod1 == 5):
         print('пасхалОчка')
     else:
---
+++ mytest.py
@@ -0,0 +1,10 @@
+import random
+############################################
+wordIAm = ["I ","You ","We ","Me "]
+wordNeedThink = ["Need ","Think ","Have "]
+wordFood = ["Apple ","Melone ","Cherry"]
+a = random.randrange(0,3)
+b = random.randrange(0,2)
+c = random.randrange(0,2)
+print(wordIAm[a],wordNeedThink[b],wordFood[c])
+print(a)
