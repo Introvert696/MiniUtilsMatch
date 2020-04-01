7ff96a7108588a3c2fdfbf03e79bbf9298b8fdbb
Evgeniy <36341190+Introvert696@users.noreply.github.com>
2020-04-01 18:54:11 +0400

    ver 0.1
    Допилил функционал
    Допилил выбор

--- firstutil.py
+++ firstutil.py
@@ -21,11 +21,18 @@ def umnozhenie(a,b):
     res = str(result)
     print("Ответ: ", res)
 #############################
-def pifagor(a,b):
+def pifagor():
+    try:
+        a = int(input('a = '))
+        b = int(input('b = '))
+    except (TypeError,ValueError):
+        print("Введите число:")
+        return pifagor()
     cen = float(sqrt(a**2 + b**2))
     cen = round(cen, 3)                 #Округление до 3 знаков после точки
     c = str(cen)
     print("Результат: ",c)
+    return main()
 ###############################
 def logarifm(x,y):
     resultlog = float(log(x, y))
@@ -33,16 +40,32 @@ def logarifm(x,y):
     relog = str(resultlog)
     print("Result", relog)
 ################################
-def ploshad(a,b,c,d):
-    ploshad = a * b * c * d
-    ploshad = str(ploshad)
-    print("Результат: ",ploshad)
+def ploshad():
+    try:
+        a = int(input('1я сторона = '))
+        b = int(input('2я сторона = '))
+        c = int(input('3я сторона = '))
+        d = int(input('4я сторона = '))
+    except (TypeError,ValueError):
+        print("Введите число:")
+        return ploshad()
+    cen = a * b * c * d
+    cen = round(cen, 3)                 #Округление до 3 знаков после точки
+    c = str(cen)
+    print("Результат: ",c)
+    return main()
 #################################
-def ploshadkrug(res):
+def ploshadkrug():
+    try:
+        res = int(input("Введите радиус круга: "))
+    except (TypeError,ValueError):
+        print('Введите коректный ответ:')
+        return ploshadkrug()
     pis = 3.14
     otvet = float(2 * pis * res)
     otvet = str(otvet)
     print("Радиус приблизительно: ",otvet)
+    return main()
 ##################################
 #############################################################################
 #############################################################################
@@ -69,80 +92,114 @@ def vremya(c,b):
     vrem = c / b
     print("Ответ", vrem)
 
-    #############################################################################
-    #############################################################################
-    #############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+def vibor():                                  #Выбор и проверка num_1 и num_2
+    try:                                    
+        num_1 = int(input("Введите пераое число: >"))
+        num_2 = int(input("Введите второе число: >"))
+        return num_1, num_2
+    except (TypeError,ValueError):
+        print('Введите цифры')
+        return vibor()
+##########################################################################
+def oneVibor():             #Выбор 1 значения vvod
+    try:
+        vvod = int(input("\n>"))
+        return vvod
+    except (TypeError,ValueError):
+        print("Введите число")
+        return oneVibor()
 
-print("Выберете что вам нужно")
-print("1.Простое вычисление")
-print("2.Разные формулы") #Допилить другие функции
-vvod = int(input("\n>"))
-if(vvod == 1):
-    print("It's work")
-    num_1 = int(input("Введите пераое число: >"))
-    num_2 = int(input("Введите второе число: >"))
-    print("Что с ними сделать: ")
-    print("1.Сложить \n2.Отнять \n3.Разделить \n4.Умножить")
-    vvod = int(input("> "))
-    if(vvod == 1):                  #НАЧАЛО Все операции
-        slozhenie(num_1,num_2)
-    elif (vvod == 2):
-        otnyatie(num_1,num_2)
-    elif (vvod == 3):
-        raznost(num_1,num_2)
-    elif (vvod == 4):
-        umnozhenie(num_1,num_2)
-    else:
-        print("Netu")                   #КОНЕЦ Все операции
-elif(vvod == 2):
-    #print("В разработке")
-    print('Выберете что вам нужно: ')
-    print("1.Теорема пифагора \n2.Логарифм \n3.Площадь 4-х угольника \n4.Площадь круга\n")
-    print("5.Нахожедние скорости/времени/расстояния")
-    vvod1 = int(input("> "))
-    if (vvod1 == 1):
-        print('Введите 2 стороны прямоугольного треугольника\n')
-        a = int(input('a = '))
-        b = int(input('b = ')) 
-        pifagor(a,b)
-    elif (vvod1 == 2):
-        print('Логарифм числа x по основанию y\n')
-        x = int(input('Введите x: \n> '))
-        y = int(input('Введите y: \n> '))
-        logarifm(x,y)
-    elif (vvod1 == 3):
-        print('Площадь 4-х угольника \nДля начала введите ваши 4 стороны')
-        a = int(input("Введите 1 сторону: "))
-        b = int(input("Введите 2 сторону: "))
-        c = int(input("Введите 3 сторону: "))
-        d = int(input("Введите 4 сторону: "))
-        ploshad(a,b,c,d)
-    elif (vvod1 == 4):
-        print('Площадь круга')
-        res = int(input("Введите радиус круга: "))
-        ploshadkrug(res)
-    elif (vvod1 == 5):
-        print('Выберете что именно: \n1.Скорость\n2.Растояние\n3.Время')
-        a = input('>')
-        a = int(a)
-        if (a == 1):
-            print('Нахождение скорости:')
-            c = int(input("Введите растояние: > "))
-            d = int(input("Введите время: >"))
-            skorost(c,d)
-        elif(a == 2):
-            print('Нахождение Расстояния: ')
-            b = int(input("Введите скорость (км/ч): >"))
-            d = int(input("Введите время: >"))
-            rasstoyanie(b,d)
-        elif (a == 3):
-            print('Нахождения Времени:')
-            c = int(input("Введите растояние: > "))
-            b = int(input("Введите скорость (км/ч): >"))
-            vremya(c,b)
+####################################################
+def main():
+    print("Выберете что вам нужно")
+    print("1.Простое вычисление")
+    print("2.Разные формулы") #Допилить другие функции
+    print("3.Выход")
+    vvod = oneVibor()
+    if(vvod == 1):
+        ######################################
+        """
+        try:
+            num_1 = int(input("Введите пераое число: >"))
+            num_2 = int(input("Введите второе число: >"))
+        except (TypeError,ValueError):
+            pass
+        """
+        ##########################################
+        print("Простое вычисление: ")
+        massvib = vibor()
+        #print(massvib)        просмотр массива
+        
+        num_1 = massvib[0]
+        num_2 = massvib[1]
+
+        print("Что с ними сделать: ")
+        print("1.Сложить \n2.Отнять \n3.Разделить \n4.Умножить")
+        vvod = oneVibor()
+        if(vvod == 1):                  #НАЧАЛО Все операции
+            slozhenie(num_1,num_2) 
+            return main()         #Сложение
+        elif (vvod == 2):
+            otnyatie(num_1,num_2)
+            return main()
+        elif (vvod == 3):
+            raznost(num_1,num_2)
+            return main()
+        elif (vvod == 4):
+            umnozhenie(num_1,num_2)
+            return main()
         else:
-            print('Nothing')
+            print("Netu")
+            return main()                  #КОНЕЦ Все операции
+    elif(vvod == 2):
+        print('Выберете что вам нужно: ')
+        print("1.Теорема пифагора \n2.Логарифм \n3.Площадь 4-х угольника \n4.Площадь круга\n")
+        print("5.Нахожедние скорости/времени/расстояния")
+        vvod = oneVibor()
+        if (vvod == 1):
+            print('Введите 2 стороны прямоугольного треугольника\n') 
+            pifagor()
+        elif (vvod == 2):
+            print('Логарифм числа x по основанию y\n')
+            x = int(input('Введите x: \n> '))
+            y = int(input('Введите y: \n> '))
+            logarifm(x,y)
+        elif (vvod == 3):
+            print('Площадь 4-х угольника \nДля начала введите ваши 4 стороны')
+            ploshad()
+            return main()
+        elif (vvod == 4):
+            print('Площадь круга')
+            ploshadkrug()
+        elif (vvod == 5):
+            print('Выберете что именно: \n1.Скорость\n2.Растояние\n3.Время')
+            a = input('>')
+            a = int(a)
+            if (a == 1):
+                print('Нахождение скорости:')
+                c = int(input("Введите растояние: > "))
+                d = int(input("Введите время: >"))
+                skorost(c,d)
+            elif(a == 2):
+                print('Нахождение Расстояния: ')
+                b = int(input("Введите скорость (км/ч): >"))
+                d = int(input("Введите время: >"))
+                rasstoyanie(b,d)
+            elif (a == 3):
+                print('Нахождения Времени:')
+                c = int(input("Введите растояние: > "))
+                b = int(input("Введите скорость (км/ч): >"))
+                vremya(c,b)
+            else:
+                print('Nothing')
+        else:
+            print("Нету такого ответа")
+    elif(vvod == 3):
+        print("ПОКА")
     else:
-        print("Нету такого ответа")
-else:
-    print("Not working")
+        print("Not working")
+        return main()
+main()
--- mytest.py
+++ mytest.py
@@ -10,9 +10,29 @@ b = random.randrange(0,2)
 c = random.randrange(0,2)
 print(wordIAm[a],wordNeedThink[b],wordFood[c])
 print(a)
-"""
+
 def abcd(a,b):
     res = a + b
     return res
 
 print(abcd(2,3))
+
+def test():
+    try:
+        i = int(input("Number"))
+    except (ValueError, TypeError):
+        print("Nepravilno")
+        return test()
+test()
+"""
+
+def vibor():
+    num_1 = int(input("Введите пераое число: >"))
+    num_2 = int(input("Введите второе число: >"))
+    return num_1, num_2
+
+print(vibor())
+mass = vibor()
+num_1 = mass[0]
+num_2 = mass[1]
+print(num_1,num_2)
