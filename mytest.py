6ee84a749f9d5b9c86884e4b43f74e73994f490b
Evgeniy <36341190+Introvert696@users.noreply.github.com>
2020-04-01 16:34:34 +0400

    !!!
    Добавил нахождеия скорости

--- mytest.py
+++ mytest.py
@@ -1,5 +1,7 @@
 import random
+
 ############################################
+"""
 wordIAm = ["I ","You ","We ","Me "]
 wordNeedThink = ["Need ","Think ","Have "]
 wordFood = ["Apple ","Melone ","Cherry"]
@@ -8,3 +10,9 @@ b = random.randrange(0,2)
 c = random.randrange(0,2)
 print(wordIAm[a],wordNeedThink[b],wordFood[c])
 print(a)
+"""
+def abcd(a,b):
+    res = a + b
+    return res
+
+print(abcd(2,3))
